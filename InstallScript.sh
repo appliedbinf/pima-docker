d96e5c3462a1b822525ee37896918ad37c0ebe66
3EVE3 <nolan.j.english@gmail.com>
2022-01-12 15:36:00 -0500

    Expanding the installation script to add docker group

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Install all the Docker Environment
 sudo apt-get update
 
 sudo apt-get install -y \
@@ -15,19 +16,27 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
+# Install the Nvidia Toolkit and Nvidia Drivers
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
+# Create docker user group and add current user to it
 
+user=$SUDO_USER
+echo $user
+
+sudo groupadd docker
+sudo usermod -aG docker $user
+sudo newgrp docker
 sudo service docker restart
 
-sudo docker pull kyxsune/pima-docker:latest
+# Download docker image from Dockerhub
+docker pull kyxsune/pima-docker:latest
 
+# Add shortcut aliasing
 printf 'alias pima="docker run --gpus all -it --mount type=bind,source=$PWD/Workdir,target=/DockerDir/Workdirectory kyxsune/pima-docker:latest"' >> ~/.bashrc
 source ~/.bashrc
 
