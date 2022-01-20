00d936366350824b47fe7aef23bb175117b6a925
Kyxsune <Kyxsune@users.noreply.github.com>
2022-01-20 01:07:55 -0500

    Updated Pima.sh Script

--- pima.sh
+++ pima.sh
@@ -7,8 +7,16 @@ do
         f) Fast5=${OPTARG};;
         r) reference=${OPTARG};;
         m) mutation=${OPTARG};;
+        *)
+                        echo 'Error in command line parsing' >&2
+                        exit 1
     esac
 done
+
+if [ -z "$Fast5" ] || [ -z "$reference" ] || [ -z "$mutation" ]; then
+        echo 'Some paths not defined' >&2
+        exit 1
+fi
 
 echo "FAST5: $Fast5";
 echo "reference: $reference";
@@ -17,4 +25,4 @@ echo "mutation: $mutation";
 CORE_PATH="/home/DockerDir/mountpoint";
 
 
-sudo docker run -it --gpus all --mount type=bind,source=$PWD,target=$CORE_PATH kyxsune/pima-docker:kraken --out $CORE_PATH/out/ --ont-fast5 $CORE_PATH/$Fast5 --threads 20 --overwrite --contamination --reference-genome=$CORE_PATH/$reference --mutation-regions=$CORE_PATH/$mutation --genome-size 5.4m --verb 3
+sudo docker run -it --gpus all --mount type=bind,source=$PWD,target=$CORE_PATH appliedbioinformaticslab/pima-docker:kraken --out $CORE_PATH/out/ --ont-fast5 $CORE_PATH/$Fast5 --threads 20 --overwrite --contamination --reference-genome=$CORE_PATH/$reference --mutation-regions=$CORE_PATH/$mutation --genome-size 5.4m --verb 3
