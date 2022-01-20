e615982d8dac0c2edf82501ad46ec106afee21bf
Kyxsune <Kyxsune@users.noreply.github.com>
2022-01-20 11:47:00 -0500

    Update InstallScript.sh

--- InstallScript.sh
+++ InstallScript.sh
@@ -12,6 +12,8 @@ sudo apt-get install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+wget https://raw.githubusercontent.com/appliedbinf/pima-docker/main/pima.sh
+
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
