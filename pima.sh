4e0e9249ee32c77bde3fa92eb29c68006fe5b318
nolan <nenglish@gatech.edu>
2022-01-21 20:10:21 -0500

    Updated the Pima.sh and Dockerfile scripts. The pima.sh script adds behaviour for the ont-fast5

--- pima.sh
+++ pima.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-while getopts m:r:f: flag
+while getopts m:r:5:q: flag
+
+CORE_PATH="/home/DockerDir/mountpoint";
 
 do
     case "${flag}" in
-        f) Fast5=${OPTARG};;
+        5) Fast5=${OPTARG};;
+        q) Fastq=${OPTARG};;
         r) reference=${OPTARG};;
         m) mutation=${OPTARG};;
         *)
@@ -13,16 +16,37 @@ do
     esac
 done
 
-if [ -z "$Fast5" ] || [ -z "$reference" ] || [ -z "$mutation" ]; then
-        echo 'Some paths not defined' >&2
+if [ -z "$reference" ] || [ -z "$mutation" ]; then
+        echo 'reference nor mutation defined ' >&2
         exit 1
 fi
 
-echo "FAST5: $Fast5";
-echo "reference: $reference";
-echo "mutation: $mutation";
+if [ -z "$Fastq" ] || [ -z "$Fast5" ]; then
+        echo 'Neither Fastq nor Fast5 file paths defined ' >&2
+        exit 1
+fi
 
-CORE_PATH="/home/DockerDir/mountpoint";
+if [ -n "$Fastq" ] && [ -n "$Fast5" ]; then
+        echo 'Both Fastq and Fast5 defined, please select only one ' >&2
+        exit 1
+fi
 
+if [ -n "$Fastq" ] && [ -z "$Fast5" ]; then
+        echo 'Executing Fastq path for pima' >&2
+        echo "FASTq: $Fastq";
+        echo "reference: $reference";
+        echo "mutation: $mutation";
 
-sudo docker run -it --gpus all --mount type=bind,source=$PWD,target=$CORE_PATH appliedbioinformaticslab/pima-docker:kraken --out $CORE_PATH/out/ --ont-fast5 $CORE_PATH/$Fast5 --threads 20 --overwrite --contamination --reference-genome=$CORE_PATH/$reference --mutation-regions=$CORE_PATH/$mutation --genome-size 5.4m --verb 3
+        sudo docker run -it --gpus all --mount type=bind,source=$PWD,target=$CORE_PATH appliedbioinformaticslab/pima-docker:kraken --out $CORE_PATH/out/ --ont-fastq $CORE_PATH/$Fastq --threads 20 --overwrite --contamination --reference-genome=$CORE_PATH/$reference --mutation-regions=$CORE_PATH/$mutation --genome-size 5.4m --verb 3
+        exit 0
+fi
+
+if [ -n "$Fastq" ] && [ -z "$Fast5" ]; then
+        echo 'Executing Fast5 path for pima' >&2
+        echo "FAST5: $Fast5";
+        echo "reference: $reference";
+        echo "mutation: $mutation";
+
+        sudo docker run -it --gpus all --mount type=bind,source=$PWD,target=$CORE_PATH appliedbioinformaticslab/pima-docker:kraken --out $CORE_PATH/out/ --ont-fast5 $CORE_PATH/$Fast5 --threads 20 --overwrite --contamination --reference-genome=$CORE_PATH/$reference --mutation-regions=$CORE_PATH/$mutation --genome-size 5.4m --verb 3
+        exit 0
+fi
